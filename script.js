fee3de3bf0afa6ee831cb72e40b691a6c4fe09ed
Monkata96 <sieroteev@gmail.com>
2019-09-12 15:14:10 +0300

    Added cookies to the browser.

--- script.js
+++ script.js
@@ -25,11 +25,55 @@ $('.slider-container').slick({
 
 /** vanilla js start */
 
-const hamburgerMenu = document.querySelector('.hamburger-menu'),
-    dropdownNavBar = document.querySelector('.nav-bar');
+function hamburgerMenu() {
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const dropdownNavBar = document.querySelector('.nav-bar');
 
-hamburgerMenu.addEventListener('click', () => {
+    hamburgerMenu.addEventListener('click', toggleHamburgerMenu);
 
-    dropdownNavBar.classList.toggle('changeNavBar');
-    hamburgerMenu.classList.toggle('hamburgerMenu');
-});
+    function toggleHamburgerMenu(e) {
+        dropdownNavBar.classList.toggle('changeNavBar');
+        hamburgerMenu.classList.toggle('hamburgerMenu');
+    };
+};
+
+function cookies() {
+    (function() {
+        const cookiesContainer = document.querySelector('.cookies');
+
+        let getCookies = document.cookie.split('=');
+
+        getCookies.forEach((item, index) => {
+            if (item === 'isAccepted') {
+                cookiesContainer.classList.add('hideCookiesContainer');
+            };
+        });
+    })();
+
+    const acceptCookiesBtn = document.querySelector('.accept');
+    const closeCookiesBtn = document.querySelector('.fa-times-circle');
+
+    acceptCookiesBtn.addEventListener('click', acceptCookiesMSg);
+    closeCookiesBtn.addEventListener('click', closeCookiesMsg);
+
+    function acceptCookiesMSg(e) {
+        const cookiesContainer = document.querySelector('.cookies');
+        let getCookies = document.cookie.split('=');
+
+        getCookies.forEach((item, index) => {
+            if (item !== 'isAccepted') {
+                console.log('yes');
+                document.cookie = 'isAccepted=yes' + '; expires=' + new Date(9999, 12, 31).toUTCString();
+                cookiesContainer.classList.add('hideCookiesContainer');
+            };
+        });
+    };
+
+    function closeCookiesMsg(e) {
+        const cookiesContainer = document.querySelector('.cookies');
+        cookiesContainer.classList.add('hideCookiesContainer');
+    };
+};
+
+hamburgerMenu();
+cookies();
