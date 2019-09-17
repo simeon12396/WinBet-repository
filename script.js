bdb12da3755821b34b74547a693490a1e8dd0333
Monkata96 <sieroteev@gmail.com>
2019-09-17 14:37:12 +0300

    Added different validations into the register form.

--- script.js
+++ script.js
@@ -90,14 +90,6 @@ function registerForm() {
 
         registerOverlay.classList.add('showOverlay');
 
-        let dataAges = {
-            'days': [],
-            'months': ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November'],
-            'allYears': function() {
-                let allYears
-            }
-
-        }
         const closeOverlay = document.querySelector('.close-overlay');
 
         closeOverlay.addEventListener('click', () => {
@@ -107,3 +99,101 @@ function registerForm() {
 };
 
 registerForm();
+
+(function dataForTheAges() {
+    let dataAges = {
+        'days': function() {
+            let arrayOfDays = [];
+            let lastDayInTheMonth = 31;
+
+            for (let i = 1; i <= lastDayInTheMonth; i++) {
+                arrayOfDays.push(i);
+            };
+
+            return arrayOfDays;
+        },
+        'months': ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        'allYears': function() {
+            let arrayOfAllYears = [];
+
+            let presentYear = new Date().getFullYear();
+
+            for (let i = 1950; i <= presentYear; i++) {
+                arrayOfAllYears.push(i);
+            };
+
+            return arrayOfAllYears;
+        }
+    };
+
+    insertSelectOptions(dataAges.days(), dataAges.months, dataAges.allYears());
+})();
+
+
+function insertSelectOptions(days, months, allYears) {
+    let getDays = days;
+    let dayOption = `<option> Select </option>`;
+    let selectForTheDays = document.querySelector('.days');
+
+    for (let key in getDays) {
+        dayOption += `<option value='${getDays[key]}'>${getDays[key]}</option>`
+    };
+
+    selectForTheDays.innerHTML = dayOption;
+
+    let getMonths = months;
+    let monthOption = `<option> Select </option>`;
+    let selectForTheMonth = document.querySelector('.months');
+
+    for (let key in getMonths) {
+        monthOption += `<option value='${getMonths[key]}'>${getMonths[key]}</option>`
+    };
+
+    selectForTheMonth.innerHTML = monthOption;
+
+    let getAllYears = allYears;
+    let yearOption = `<option> Select </option>`;
+    let selectForTheYears = document.querySelector('.all-years');
+
+    for (let key in getAllYears) {
+        yearOption += `<option value='${getAllYears[key]}'> ${getAllYears[key]} </option>`
+    }
+
+    selectForTheYears.innerHTML = yearOption;
+};
+
+function submitRegisterForm() {
+    let submitBtn = document.querySelector('.submit-btn');
+
+    submitBtn.addEventListener('click', submitForm);
+
+    function submitForm(e) {
+        e.preventDefault();
+
+        console.log(e);
+        const inputName = e.target.form[0].value;
+        const inputEmail = e.target.form[1].value;
+
+        const inputPassword = e.target.form[2].value;
+        const inputConfirmPassword = e.target.form[3].value;
+
+
+        const inputSelectBoxDay = parseInt(e.target.form[4].value);
+        const inputSelectBoxMonth = parseInt(e.target.form[5].value);
+        const inputSelectBoxYear = parseInt(e.target.form[6].value);
+
+        const minYear = new Date().getFullYear() - 18;
+
+        if ((inputSelectBoxYear <= minYear) &&
+            (inputName.charAt(0) == 'S') &&
+            (inputName.length >= 4) &&
+            (inputPassword === inputConfirmPassword)
+        ) {
+            alert('Congrats')
+        } else {
+            alert('Correctly fill the form')
+        }
+    };
+};
+
+submitRegisterForm();
